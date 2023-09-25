f3724365ac8ed1a94323b2175034fb582bb73ac5
0skarmp <omoralespj@gmail.com>
2023-09-25 23:58:30 +0000

    task 1 , script to concat 2 strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/bin/bash/env python
+"""Script to concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concatenate two strings and return the result as a string"""
+    return str1 + str2
