bd96bfc7c3fe0acf7122d0a55a0b9759f39d4a25
0skarmp <omoralespj@gmail.com>
2023-09-26 00:44:50 +0000

    taks 5, add library

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,9 @@
 """Script to compute the sum of a list of floats."""
 
 
+from typing import List
+
+
 def sum_list(input_list: list[float]) -> float:
     """Function to calculate the sum of a list of floats."""
     return sum(input_list)
