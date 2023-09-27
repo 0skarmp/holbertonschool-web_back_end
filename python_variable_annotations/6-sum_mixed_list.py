22ac395ac48f73a66814f6ac7a1674ec9780cd33
0skarmp <omoralespj@gmail.com>
2023-09-27 06:07:41 +0000

    task 6, script to return mixed numbers 'float & int'

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/env python3
+"""scrip to return a float of mix int and float"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst List[Union[int, float]]) -> float:
+    """Sums the elements in a list of integers and floats"""
+    for nums_mixed in mxd_lst:
+        n_float = 0
+        n_float += float(nums_mixed)
+        return n_float
