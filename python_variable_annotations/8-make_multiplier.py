d64933da630ad1c6453cde03cbc794e938c2f938
0skarmp <omoralespj@gmail.com>
2023-09-27 06:44:48 +0000

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""scritp to multply a float * float"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[float], float]:
+    """Returns function that multiplies a float by the specified multiplier"""
+    def function_to_multiply(x: float) -> float):
+        return: x * multiplier
+    return function_to_multiply
