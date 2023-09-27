fa755a819bf2412caec0d03e1f82c02991af7bf1
0skarmp <omoralespj@gmail.com>
2023-09-27 06:26:55 +0000

    task 7 , scritp to return tuple plus square

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" scrit to retunr a square and tuple of str and float"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v Union[int, float]) -> tuple(str, float):
+    """Takes a string and an int or float and returns a tuple"""
+    square = float(v) ** 2
+    return k, square
