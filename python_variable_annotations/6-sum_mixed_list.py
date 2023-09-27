2c06170e899ecfb412b9d5b983a2d6471897e261
0skarmp <omoralespj@gmail.com>
2023-09-27 06:10:44 +0000

    fix bugs, add ':' between the numbers

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sums the elements in a list of integers and floats"""
     for nums_mixed in mxd_lst:
         n_float = 0
