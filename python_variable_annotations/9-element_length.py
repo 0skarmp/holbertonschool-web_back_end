a1f0c4ba6b20c34f2e93153109c60a5c04635c1d
0skarmp <omoralespj@gmail.com>
2023-09-27 06:57:56 +0000

    fix bugs, indetation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """scritp to  type an iterable object"""
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lstIterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Takes a list of strings and returns a list of tuples"""
-     return [(i, len(i)) for i in lst]
+    return [(i, len(i)) for i in lst]
