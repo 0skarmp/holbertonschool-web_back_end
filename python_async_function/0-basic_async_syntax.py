7ed861bf1eaa2ba4084871580039baac77f7361a
0skarmp <omoralespj@gmail.com>
2023-09-29 07:44:11 +0000

    fix bugs, correct the name of the function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def asynchronus(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous function that generates a random
         float and sleeps for a random
         duration before returning the generated float."""
