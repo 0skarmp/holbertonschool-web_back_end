6ce79d16337079add3e959317edc3af64b2a748d
0skarmp <omoralespj@gmail.com>
2023-09-29 07:33:28 +0000

    add task 0 asycn fucntion

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""async functions """
+import asyncio
+import random
+
+
+async def asynchronus(max_delay: int = 10) -> float:
+    """Asynchronous function that generates a random
+        float and sleeps for a random
+        duration before returning the generated float."""
+    float and sleeps for a randon
+    duration before returning the generated float.
+    aleatory = random.uniform(0, max_delay)
+    await asyncio.sleep(aleatory)
+    return aleatory
