aa44996205c271cd9f36b4ff6d0b4dc43860f6c6
0skarmp <omoralespj@gmail.com>
2023-09-29 07:38:23 +0000

    fix sintax

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,8 +8,6 @@ async def asynchronus(max_delay: int = 10) -> float:
     """Asynchronous function that generates a random
         float and sleeps for a random
         duration before returning the generated float."""
-    float and sleeps for a randon
-    duration before returning the generated float.
     aleatory = random.uniform(0, max_delay)
     await asyncio.sleep(aleatory)
     return aleatory
