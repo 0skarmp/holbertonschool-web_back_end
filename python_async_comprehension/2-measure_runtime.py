3988834453b5eb3fe4881af62518a0ae6634845a
0skarmp <omoralespj@gmail.com>
2023-10-01 23:36:48 +0000

    fix bugfs

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,8 +7,8 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """Execute async_comprehension 4 times in parallel using asynciogather"""
-    time0 = time.time()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    time1 = time.time()
-    TotalT = time1 - time0
-    return TotalT
+    start = time.time()
+    await asyncio.gather(*[async_comprehension(), async_comprehension(),
+                           async_comprehension(), async_comprehension()])
+    total_runtime = time.time() - start
+    return total_runtime
