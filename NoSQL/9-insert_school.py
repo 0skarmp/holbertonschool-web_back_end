f26679cc41fffea63fbbd5b605a3ff1d50a2a7b5
0skarmp <omoralespj@gmail.com>
2023-10-04 14:31:07 -0700

    script to insert a document in the collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""script to insert"""
+from pymongo import mongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """scritp to insert documents in collection"""
+    result = mongo_collection.insert_one()
+    return (result.inserted_id)
