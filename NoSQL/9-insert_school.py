9b0d16909c2d0354c271b2960466cc4960632a9b
0skarmp <omoralespj@gmail.com>
2023-10-04 19:47:30 -0700

    fix bugs, typo

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ from pymongo import MongoClient
 def insert_school(mongo_collection, **kwargs):
     """scritp to insert documents in collection"""
     result = mongo_collection.insert_one(kwargs)
-    return (result.inserted_id)
+    return result.inserted_id
