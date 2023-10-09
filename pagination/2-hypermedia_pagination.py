81675898a7456837f562b531a609c73f2fb4b317
0skarmp <omoralespj@gmail.com>
2023-10-08 22:00:45 -0700

    task2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -52,3 +52,19 @@ class Server:
             return []
 
         return [self.dataset()[i] for i in range(first_index, last_index)]
+
+     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ return a dictionary
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+                    'page_size': len(self.get_page(page, page_size)),
+                    'page': page,
+                    'data': self.get_page(page, page_size),
+                    'next_page': next_page,
+                    'prev_page': prev_page,
+                    'total_pages': total_pages
+                }
