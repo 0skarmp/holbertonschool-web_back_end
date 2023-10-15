49866a4af8ed30074c7f74ce1fd838ac3cb367d1
0skarmp <omorales@gmail.com>
2023-10-15 13:14:02 -0700

    tasl 0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,3 +1,3 @@
 export default class ClassRoom{
-	constructor(maxStudentsSize){
-		this._maxStudentsSize = maxStudentsSize;}}
+  constructor(maxStudentsSize){
+    this._maxStudentsSize = maxStudentsSize;}}
