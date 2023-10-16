0c912f0f3a9b5d6a29185b4bc31d6433d8b5846e
0skarmp <omorales@gmail.com>
2023-10-15 20:39:43 -0700

    task3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,37 +1,31 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('Name must be a string');
-    if (typeof length !== 'number') throw TypeError('Length must be a number');
-    if (!Array.isArray(students)) throw TypeError('Students must be an array');
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
     this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
+  // get method to code
+  get code() {
+    return this._code;
+  }
+
+  // set method code
+  set code(value) {
+    this._code = value;
+  }
+
+  // get method to name
   get name() {
     return this._name;
   }
 
+  // set method name
   set name(value) {
-    if (typeof value === 'string') this._name = value;
-    else throw TypeError('Name must be a string');
+    this._name = value;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value === 'number') this._length = value;
-    else throw TypeError('Length must be a number');
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(value) {
-    if (Array.isArray(value)) this._students = value;
-    else throw TypeError('Students must be an array');
+  // new method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
