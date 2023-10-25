03c8b969320e1f6b344c0611848b7b45f1231c37
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-10-25 16:59:03 -0500

    Update

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) { 
-        resolve({
-          status: 200,
-          body: "success"
-        });
-      } else {
-        reject("The fake API is not working currently");
-      }
-    });
-  }
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: "success"
+      });
+    } else {
+      reject("The fake API is not working currently");
+    }
+  });
+}
