9a821f315ca3b1dce2b611dbfd36a00705fc09d1
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-10-26 03:36:16 -0500

    Update 8-try.js

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,6 @@
-async function divideFunction(numerator, denominator) {
+export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   }
-
   return numerator / denominator;
 }
